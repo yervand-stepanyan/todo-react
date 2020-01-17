4db72908b3fac35fbc60654620473c2738c9a64f
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-01-17 19:11:51 +0400

    Updates

---
+++ precache-manifest.e61dd06ac6482510147884065f5dff1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64977f06ce24784dda6eb7d53b4eb628",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "2ccb0955e32487dd0b16",
+    "url": "/todo-react/static/css/main.7d36d573.chunk.css"
+  },
+  {
+    "revision": "a1d328bee218b38a62ee",
+    "url": "/todo-react/static/js/2.f1f6720e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-react/static/js/2.f1f6720e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ccb0955e32487dd0b16",
+    "url": "/todo-react/static/js/main.76f830fe.chunk.js"
+  },
+  {
+    "revision": "8b015e0126189823d40b",
+    "url": "/todo-react/static/js/runtime-main.7a204f65.js"
+  }
+]);
